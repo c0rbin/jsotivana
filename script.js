357a36c74fb2e44885383786f21c41dde1ee2981
Dinis <drbrauz@mail.ru>
2019-12-22 21:19:57 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict';
+a = 5;
+console.log(a);
+// let answer = confirm("Are you here?");
+// console.log(answer);
+// let answer = +prompt("Есть ли Вам 18?", "Нет");
+// console.log(typeof(answer));
+// console.log("arr" + " -object");
+// console.log(5 + " -object");
+// let incr = 10,
+//     decr = 10;
+
+// console.log(incr++);
+// console.log(decr--);
+// console.log(5%2);
